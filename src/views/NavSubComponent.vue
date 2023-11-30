226386b780eee8de744169b55578326933fdfa11
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2023-11-30 10:32:21 +0000

    🎉 Initial commit

---
+++ src/views/NavSubComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Tab 1</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+
+    <IonContent class="ion-padding">
+        <IonLabel >
+            {{ msg }}
+        </IonLabel>
+    </IonContent>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonHeader, IonLabel, IonTitle, IonToolbar } from '@ionic/vue';
+
+defineProps<{
+    msg: string
+}>()
+</script>
---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <!-- Works with params in camelCase 👍 -->
+      <!-- <IonNav :root="NavSubComponent" :rootParams="message"/> -->
+
+      <!-- Doesn't work with kebab-case while it should 💥 -->
+      <IonNav :root="NavSubComponent" :root-params="message"/>
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonNav } from '@ionic/vue';
+
+import NavSubComponent from './NavSubComponent.vue'
+
+const message = {
+  msg: 'Hello!'
+}
+</script>
